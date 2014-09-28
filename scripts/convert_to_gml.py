f0d820e29d01ecab1d4d2b6cbaf39ce1e335bc6a
fatiherikli <fatih@hipo.biz>
2014-09-28 06:05:45 +0300

    Implement graph interface

--- scripts/convert_to_gml.py
+++ scripts/convert_to_gml.py
@@ -5,6 +5,8 @@ import networkx as nx
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+database = json.load(open("../data/data.json"))
+
 GROUP_MAPPING = {
     "Influenced by": "Programming Language",
     "Designed by": "Computer Scientist",
@@ -20,11 +22,22 @@ network = {
 }
 
 
+def calculate_node_weight(label):
+    weight = 1
+    data = database.get(label)
+
+    if data and "Influenced" in data:
+        weight += len(data["Influenced"])
+
+    return weight
+
+
 def add_node(label, group):
     node = {
         "name": label,
         "id": len(network["nodes"]),
-        "group": group
+        "group": group,
+        "weight": calculate_node_weight(label)
     }
     network["nodes"].append(node)
     return node
@@ -33,30 +46,33 @@ def add_node(label, group):
 def find_node_by_label(label):
     for node in network["nodes"]:
         if node["name"] == label:
-            return node["id"]
+            return node
 
 
 def main():
-    data = json.load(open("../data/data.json"))
 
-    for language, properties in data.items():
+    for language, properties in database.items():
 
-        add_node(language, GROUP_MAPPING["Influenced by"])
+        source_node = add_node(language, GROUP_MAPPING["Influenced by"])
 
         for edge_type, group in GROUP_MAPPING.iteritems():
             if edge_type in properties:
                 nodes = properties[edge_type]
 
                 for node in nodes:
-                    node_id = find_node_by_label(node)
-                    if node_id is None:
-                        added_node = add_node(node, group)
-                        node_id = added_node["id"]
+
+                    if not node:
+                        continue
+
+                    target_node = find_node_by_label(node)
+
+                    if not target_node:
+                        target_node = add_node(node, group)
 
                     network["links"].append({
-                        "source": find_node_by_label(language),
-                        "target": node_id,
-                        "type": edge_type
+                        "source": source_node["id"],
+                        "target": target_node["id"],
+                        "type": edge_type,
                     })
 
     graph = nx.MultiDiGraph()
@@ -66,7 +82,24 @@ def main():
     for link in network["links"]:
         graph.add_edge(network["nodes"][link["source"]]["name"].encode("utf-8"),
                        network["nodes"][link["target"]]["name"].encode("utf-8"),
-                       label=link["type"])
+                       label=link["type"], weight=1)
+
+    # set weights
+    for source, target, payload in graph.edges(data=True):
+        if payload["label"] == "Designed by":
+            payload["weight"] += (graph.in_degree(target) * 4)
+
+        if payload["label"] == "Developer":
+            payload["weight"] += (graph.in_degree(target) * 2)
+
+        if payload["label"] == "Dialect":
+            payload["weight"] += 4
+
+        if payload["label"] == "Major implementations":
+            payload["weight"] += 3
+
+        if payload["label"] == "Implementation language":
+            payload["weight"] += 4
 
     nx.write_gml(graph, '../data/network.gml')
 
